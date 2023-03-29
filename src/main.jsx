d421fe3dabb7d84c0b1af0be136d3d07471b3045
José Antonio González Morente <jgonzalezmorente@gmail.com>
2023-03-29 16:55:54 +0200

    HookApp (parte II)

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { TodoApp } from './08-useReducer/TodoApp';
 import ReactDOM from 'react-dom/client';
 
 // import { HooksApp } from './HooksApp';
@@ -12,15 +13,14 @@ import ReactDOM from 'react-dom/client';
 // import { Memorize } from './06-memos/Memorize';
 // import { MemoHook } from './06-memos/MemoHook';
 // import { CallbackHook } from './06-memos/CallbackHook';
-import { Padre } from './07-tarea-memo/Padre';
+// import { Padre } from './07-tarea-memo/Padre';
+// import './08-useReducer/intro-reducer';
 
 
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-    
-    <Padre />
-    
+  // <React.StrictMode>    
+    <TodoApp></TodoApp>    
   // </React.StrictMode>,
-)
+);
